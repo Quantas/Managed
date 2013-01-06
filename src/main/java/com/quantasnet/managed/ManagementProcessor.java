9808bd27d76eacfe87c0f74a765eae5d903afd79
landsverka <dewdew@gmail.com>
2013-01-06 22:14:29 +0000

    Add static field to keep track of the registered object names Downgrade Java version to 1.5 for broader compatibility

--- src/main/java/com/quantasnet/managed/ManagementProcessor.java
+++ src/main/java/com/quantasnet/managed/ManagementProcessor.java
@@ -1,6 +1,8 @@
 package com.quantasnet.managed;
 
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.management.DynamicMBean;
 import javax.management.JMException;
@@ -17,12 +19,23 @@ public final class ManagementProcessor
 {
 	private static final Logger LOG = LoggerFactory.getLogger(ManagementProcessor.class);
 	
+	@Managed
+	private static final List<ObjectName> REGISTERED_OBJS = new ArrayList<ObjectName>();
+	
+	private static final ManagementProcessor instance;
+	
+	static
+	{
+		instance = new ManagementProcessor();
+		ManagementProcessor.register(instance, "Objects registered with the @Managed annotation");
+	}
+	
 	private ManagementProcessor()
 	{
 		// no-op
 	}
 	
-	public static ObjectName constructObjectName(Class<?> clazz)
+	public static ObjectName constructObjectName(final Class<?> clazz)
 	{
 		ObjectName name = null;
 		
@@ -47,9 +60,8 @@ public final class ManagementProcessor
 	 * Register a new DynamicMBean
 	 * @param obj Instance of the object to be managed
 	 * @param desc Description of the MBean
-	 * @throws InvalidManagementAnnotationException
 	 */
-	public static void register(Object obj, String desc)
+	public static void register(final Object obj, final String desc)
 	{
 		Object instance = null;
 		
@@ -70,7 +82,14 @@ public final class ManagementProcessor
 			
 			if(instance != null)
 			{
-				ManagementFactory.getPlatformMBeanServer().registerMBean(instance, constructObjectName(obj.getClass()));
+				final ObjectName objName = constructObjectName(obj.getClass());
+				
+				ManagementFactory.getPlatformMBeanServer().registerMBean(instance, objName);
+				
+				synchronized(REGISTERED_OBJS)
+				{
+					REGISTERED_OBJS.add(objName);
+				}
 			}
 		}
 		catch(Exception e)
@@ -79,11 +98,29 @@ public final class ManagementProcessor
 		}
 	}
 	
-	public static void unregister(Object obj)
+	public static void unregister(final Object obj)
 	{
 		try
 		{
-			ManagementFactory.getPlatformMBeanServer().unregisterMBean(constructObjectName(obj.getClass()));
+			final ObjectName objName = constructObjectName(obj.getClass());
+			ManagementFactory.getPlatformMBeanServer().unregisterMBean(objName);
+			
+			synchronized(REGISTERED_OBJS)
+			{
+				ObjectName toRemove = null;
+				for (final ObjectName oName : REGISTERED_OBJS)
+				{
+					if (oName.getCanonicalName().equals(objName.getCanonicalName()))
+					{
+						toRemove = oName;
+					}
+				}
+				
+				if(toRemove != null)
+				{
+					REGISTERED_OBJS.remove(toRemove);
+				}
+			}
 		}
 		catch(JMException e)
 		{
